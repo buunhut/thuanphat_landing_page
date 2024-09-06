6468c4677211c0e7cb111425bf28b5729e926e99
Buu Nhut <nhut.nta@gmail.com>
2024-09-06 12:14:02 +0700

    push

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,7 +3,14 @@ import React from "react";
 const ContactPage = () => {
   return (
     <div id="container">
-      <div className="content">ContactPage</div>
+      <div className="content">
+        <div className="contentItem">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.1047689287384!2d106.68301017553947!3d10.803287258696646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528dafc0ba27f%3A0x63c2abb02cb28043!2zMTE1LzI2IMSQLiBQaGFuIMSQxINuZyBMxrB1LCBQaMaw4budbmcgNywgUGjDuiBOaHXhuq1uLCBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1725599234157!5m2!1sen!2s"
+            loading="lazy"
+          ></iframe>
+        </div>
+      </div>
     </div>
   );
 };
