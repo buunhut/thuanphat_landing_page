cdf188e64150f14affc274fe37c743be4ae7456a
Buu Nhut <nhut.nta@gmail.com>
2024-09-06 11:53:35 +0700

    push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,28 @@ const Footer = () => {
         <div className="address">
           <ul>
             <li>
-              115/26 Phan Dang Luu, 7 Ward, Phu Nhuan Dictrict, Ho Chi Minh
-              City, VietName
+              <i className="fa-solid fa-house"></i>
+              <b>THUAN PHAT TECHNOLOGY TRADING COMPANY LIMITED</b>
             </li>
-            <li>0909240886</li>
-            <li>nhut.nta@gmail.com</li>
+            <li>
+              <i className="fa-solid fa-location-dot"></i>
+              115/26 Phan Dang Luu, 7 Ward, Phu Nhuan Dictrict, Ho Chi Minh
+              City, VietNam
+            </li>
+            <li>
+              <i className="fa-solid fa-phone"></i>
+              <a href="tel:+84909240886">0909 240 886</a>
+            </li>
+            <li>
+              <i className="fa-solid fa-envelope"></i>
+              <a href="mailto:nhut.nta@gmail.com">nhut.nta@gmail.com</a>
+            </li>
+            <li>
+              <i className="fa-solid fa-globe"></i>
+              <a href="https://congtythuanphat.com">
+                https://congtythuanphat.con
+              </a>
+            </li>
           </ul>
         </div>
         <div className="menu">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,59 @@ import React from "react";
 const HomePage = () => {
   return (
     <div id="container">
-      <div className="content">HomePage 1</div>
+      <div className="banner"></div>
+      <div className="content">
+        <div className="contentItem">
+          <h1>
+            <i className="fa-solid fa-users-gear"></i> About Us
+          </h1>
+          <p>
+            Our company specializes in trading and technology, offering
+            high-quality products such as computers, components, network
+            equipment, surveillance cameras, and memory cards..., In addition,
+            we provide software services, including website design, custom
+            software development, and technology consulting. With a team of
+            experienced and dedicated professionals, we are committed to
+            delivering optimal solutions that meet the technological needs of
+            our customers in the digital age.
+          </p>
+        </div>
+
+        <div className="contentItem">
+          <h1>
+            <i className="fa-solid fa-screwdriver-wrench"></i> Services
+          </h1>
+          <ul>
+            <li>
+              <b>Commerce</b>: We offer a wide range of high-quality technology
+              products, including computers, electronic components, network
+              equipment, surveillance cameras, and memory cards. All products
+              are carefully selected from reputable suppliers to meet the needs
+              of both individual and business customers.
+            </li>
+            <li>
+              <b>Website Design</b>: We design professional websites that are
+              SEO-friendly and optimized for user experience, helping your
+              business establish a strong online presence.
+            </li>
+            <li>
+              <b>Software Development</b>: We provide customized software
+              solutions tailored to the specific needs of your business, ranging
+              from enterprise management systems to mobile applications.
+            </li>
+            <li>
+              <b>Technology Consulting</b>: With a team of experienced experts,
+              we offer effective technology consulting services, helping
+              businesses streamline processes and improve operational
+              efficiency.
+            </li>
+          </ul>
+          <p>
+            Our company is committed to delivering high-quality services that
+            fully meet the technological and growth needs of our customers.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
