a1738f94756427d7222943e086b257de61c110aa
Buu Nhut <nhut.nta@gmail.com>
2024-09-14 19:57:13 +0700

    event

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -5,6 +5,7 @@ const ContactPage = () => {
     <div id="container">
       <div className="content">
         <div className="contentItem">
+          <h1>Contact</h1>
           <form action="">
             <div className="inputItem">
               <i className="fa-solid fa-user"></i>
@@ -28,6 +29,7 @@ const ContactPage = () => {
           </form>
         </div>
         <div className="contentItem">
+          <h1>Location</h1>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.1047689287384!2d106.68301017553947!3d10.803287258696646!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528dafc0ba27f%3A0x63c2abb02cb28043!2zMTE1LzI2IMSQLiBQaGFuIMSQxINuZyBMxrB1LCBQaMaw4budbmcgNywgUGjDuiBOaHXhuq1uLCBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1725599234157!5m2!1sen!2s"
             loading="lazy"
